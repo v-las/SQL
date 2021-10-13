c3138659215fc239f1e26c51b5f5632a779b843e
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-10-14 01:13:02 +0600

    task 2 update

--- HW_03/task_2_inserts.sql
+++ HW_03/task_2_inserts.sql
@@ -16,7 +16,7 @@
 --Заполнение "roles"
 --INSERT INTO roles (id, role_title) VALUES (DEFAULT, 'Area Manager');
 --INSERT INTO roles (id, role_title) VALUES (DEFAULT, 'Store Manager');
---INSERT INTO roles (id, role_title) VALUES (DEFAULT, 'Deputy Managers');
+--INSERT INTO roles (id, role_title) VALUES (DEFAULT, 'Deputy Manager');
 --INSERT INTO roles (id, role_title) VALUES (DEFAULT, 'Team Leader');
 --INSERT INTO roles (id, role_title) VALUES (DEFAULT, 'Retail Assistant');
 --INSERT INTO roles (id, role_title) VALUES (DEFAULT, 'Night Replenishment Assistant');
@@ -40,47 +40,104 @@
 --SELECT * FROM roles_salary;
 
 --Заполнение "employees"
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'John Wick');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'James Bond');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Walter White');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mata Chary');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Brandon Fraiser');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Alissya Keys');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Zoe Krawitz');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Lenny Krawitz');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Christian Bale');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Vinona Rider');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Jonah Hill');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mangomery Burnes');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Morgan Freeman');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Nicola Tesla');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Niel DeGrasse Tyson');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'John Wich');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'James Bomd');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Walter Whike');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mata Hapi');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Brendan Flaser');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Alicia Keez');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Zoe Kravipz');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Lenny Kravipz');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Christian Bane');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Winona Ryter');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Jonah Hipp');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Montgomery Turns');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Morgan Fleeman');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Nikola Testa');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Neil DeGrasse Tykoon');
 --INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Smash N. Pumpkins');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Harry Potter');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Lonardo DiCaprio');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mona Lisa');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Harry Pokker');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Leonardo PiCaprio');
+--INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mona Lida');
 --INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Elon Must');
 --SELECT * FROM employees;
 
---Заполнение "employees"
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'John Wick');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'James Bond');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Walter White');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mata Chary');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Brandon Fraiser');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Alissya Keys');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Zoe Krawitz');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Lenny Krawitz');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Christian Bale');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Vinona Rider');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Jonah Hill');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mangomery Burnes');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Morgan Freeman');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Nicola Tesla');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Niel DeGrasse Tyson');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Smash N. Pumpkins');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Harry Potter');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Lonardo DiCaprio');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Mona Lisa');
---INSERT INTO employees (id, employee_name) VALUES (DEFAULT, 'Elon Must');
---SELECT * FROM employees;
+--Заполнение "employees_roles"
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 1, 17);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 2, 13);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 2, 16);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 3, 19);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 3, 12);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 10, 14);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 9, 18);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 9, 11);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 9, 15);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 4, 20);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 5, 1);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 5, 2);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 5, 6);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 6, 7);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 6, 9);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 6, 8);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 8, 5);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 7, 4);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 7, 10);
+--INSERT INTO employees_roles (id, id_role, id_employee) VALUES (DEFAULT, 7, 3);
+--SELECT * FROM employees_roles;
+
+--Заполнение "service"
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Desktop App', 30000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Web App', 70000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Mobile App', 90000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Server', 120000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Website', 110000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'PC Game', 200000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Mobile Game', 340000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'OS', 1300500);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Social Media', 1240000);
+--INSERT INTO service (id, service_title, price) VALUES (DEFAULT, 'Calculator', 3400);
+--SELECT * FROM service;
+
+--Заполнение "materials"
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Wood', 10, 300);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Rocks', 15, 250);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Mineral', 3, 450);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Soil', 100, 700);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Water', 30, 900);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Gas', 10, 850);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Iron', 70, 150);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Plastic', 160, 400);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Ceramic', 60, 650);
+--INSERT INTO materials (id, material_title, amount, price)
+--	VALUES (DEFAULT, 'Rubber', 70, 500);
+--SELECT * FROM materials;
+
+--Заполнение "claim"
+
+--SELECT * FROM claim;
+
+CREATE TABLE claim (
+	id serial PRIMARY KEY,
+	service_id int NOT NULL,
+	employee_id int NOT NULL,
+	material_id int NOT NULL,
+	claim_date date NOT NULL,
+	sales_manager int NOT NULL,
+	FOREIGN KEY (service_id)
+		REFERENCES service (id),
+	FOREIGN KEY (employee_id)
+		REFERENCES employees (id),
+	FOREIGN KEY (material_id)
+		REFERENCES materials (id),
+	FOREIGN KEY (sales_manager)
+		REFERENCES employees (id)
+);
